bb63862c09950f37341883dcd42aaec2fd4de6a5
Huy1611 <caohuy128@gmail.com>
2024-12-01 18:36:30 +0700

    update app.js

--- Front-End-User/src/App.js
+++ Front-End-User/src/App.js
@@ -22,16 +22,13 @@ import LoadingSpinner from "./components/LoadingSpinner";
 function App() {
   const dispatch = useDispatch();
   const [cartProductCount, setCartProductCount] = useState(0);
-  const [loading, setLoading] = useState(false);
   const [dataUser, setDataUser] = useState([]);
 
   const fetchUserDetails = async () => {
-    setLoading(true);
     const dataReponse = await fetch(SummaryAip.current_user.url, {
       method: SummaryAip.method,
       credentials: "include",
     });
-    setLoading(false);
     const dataApi = await dataReponse.json();
     if (dataApi?.success) {
       setDataUser(dataApi?.data);
@@ -40,12 +37,10 @@ function App() {
   };
 
   const fetchUserAddToCart = async () => {
-    setLoading(true);
     const dataResponse = await fetch(SummaryAip.count_add_to_cart.url, {
       method: SummaryAip.count_add_to_cart.method,
       credentials: "include",
     });
-    setLoading(false);
     const dataApi = await dataResponse.json();
     setCartProductCount(dataApi?.data?.count);
   };
@@ -55,52 +50,44 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const initializeApp = async () => {
-      await fetchUserDetails();
-      await fetchUserAddToCart();
-      setTimeout(() => setLoading(false), 1000);
-    };
-
-    initializeApp();
+    fetchUserDetails();
+    fetchUserAddToCart();
   }, []);
 
   return (
     <>
-      {loading ? (
+      {/* {loading ? (
         <LoadingSpinner />
-      ) : (
-        <Context.Provider
-          value={{
-            fetchUserDetails,
-            cartProductCount,
-            dataUser,
-            fetchUserAddToCart,
-            updateCartProductCount,
-          }}
-        >
-          <TabProvider>
-            <ToastContainer position="top-right" />
-            <ProductCompareProvider>
-              <FavoritesProvider>
-                <SelectedProductsProvider>
-                  <CartProvider>
-                    <Header />
-                    <main
-                      id="main-content"
-                      className="min-h-[calc(100vh-140px)]"
-                    >
-                      <Outlet />
-                    </main>
-                    <CompareProductsModal />
-                    <Footer />
-                  </CartProvider>
-                </SelectedProductsProvider>
-              </FavoritesProvider>
-            </ProductCompareProvider>
-            <ScrollToTopButton />
-          </TabProvider>
-        </Context.Provider>
-      )}
+      ) : ( */}
+      <Context.Provider
+        value={{
+          fetchUserDetails,
+          cartProductCount,
+          dataUser,
+          fetchUserAddToCart,
+          updateCartProductCount,
+        }}
+      >
+        <TabProvider>
+          <ToastContainer position="top-right" />
+          <ProductCompareProvider>
+            <FavoritesProvider>
+              <SelectedProductsProvider>
+                <CartProvider>
+                  <Header />
+                  <main id="main-content" className="min-h-[calc(100vh-140px)]">
+                    <Outlet />
+                  </main>
+                  <CompareProductsModal />
+                  <Footer />
+                </CartProvider>
+              </SelectedProductsProvider>
+            </FavoritesProvider>
+          </ProductCompareProvider>
+          <ScrollToTopButton />
+        </TabProvider>
+      </Context.Provider>
+      {/* )} */}
     </>
   );
 }
