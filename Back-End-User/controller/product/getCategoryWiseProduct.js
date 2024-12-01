970b1c0159beabe77b42abcc95dd3f187cc3f7d4
Huy1611 <caohuy128@gmail.com>
2024-12-02 00:28:11 +0700

    update one 01-12 backend

--- Back-End-User/controller/product/getCategoryWiseProduct.js
+++ Back-End-User/controller/product/getCategoryWiseProduct.js
@@ -2,13 +2,23 @@ const productModel = require("../../model/productModel");
 
 async function getCategoryWiseProduct(req, res) {
   try {
-    const { category } = req?.body || req?.query;
-    const product = await productModel.find({ category, status: "Completed" });
+    const { category, limit = 6, page = 1 } = req?.body || req?.query;
+    const product = await productModel
+      .find({ category, status: "Completed" })
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .exec();
+    const count = await productModel.countDocuments({
+      category,
+      status: "Completed",
+    });
     res.json({
       message: "Product",
       error: false,
       success: true,
       data: product,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
     });
   } catch (error) {
     res.status(400).json({
